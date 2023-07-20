47e7b1eb4a98ef643285b82bb7ef8e2c805ee1cb
viraj2011 <107635321+viraj2011@users.noreply.github.com>
2023-07-20 19:38:12 +0530

    Add files via upload

---
+++ project-111.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+from_dir = "Downloads"
+to_dir = "Document_Files"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+print("Files in the source directory:")
+
